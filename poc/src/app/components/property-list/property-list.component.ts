f674d983d0ee76ee7be7fabcd00d06f095841406
Shaahid Sheth <55107598+shethsh1@users.noreply.github.com>
2022-07-05 19:11:54 +0000

    Added pagination

--- poc/src/app/components/property-list/property-list.component.ts
+++ poc/src/app/components/property-list/property-list.component.ts
@@ -3,25 +3,53 @@ import { fetchProperties } from 'src/app/core/properties/property.actions';
 import { Store } from '@ngrx/store'
 import { Properties } from 'src/app/core/properties/property.state';
 
+interface CurrentProperties extends Properties {
+  page : number,
+  totalPossiblePages: number,
+  limit: number,
+  offset: number
+}
+
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.scss']
 })
 export class PropertyListComponent implements OnInit {
-  properties: Properties = {
+  properties: CurrentProperties = {
     data: [],
-    totalCount: 0
+    totalCount: 0,
+    page: 1,
+    totalPossiblePages: 1,
+    limit: 10,
+    offset: 0
   };
+  numbers : number[] = []
 
 
   constructor(private store: Store<{ properties: Properties }>) { }
 
   ngOnInit(): void {
-    this.store.dispatch(fetchProperties({ limit: 10, offset: 0 }))
+    this.store.dispatch(fetchProperties({ limit: this.properties.limit, offset: this.properties.offset }))
     this.store.select('properties').subscribe((data) => {
-      this.properties = data
+
+      let totalPages : number = 1
+      while(data.totalCount > totalPages * 10) {
+        totalPages++
+      }
+
+      this.numbers = Array(totalPages).fill(0).map((x,i)=>i+1);
+
+      this.properties = {
+        ...this.properties,
+        data: data.data,
+        totalCount: data.totalCount,
+        page: 1,
+        totalPossiblePages: totalPages,
+      }
     })
+
+    
 
   }
 
@@ -32,5 +60,15 @@ export class PropertyListComponent implements OnInit {
       return true
     }
   }
+
+  setPagination(page : number) {
+    this.store.dispatch(fetchProperties({ limit: page * 10, offset: (page - 1) * 10 }))
+    this.store.select('properties').subscribe((data) => {
+      this.properties.data = data.data
+      this.properties.page = page
+    })
+
+  }
+
 
 }
